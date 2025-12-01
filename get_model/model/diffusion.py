5c00a5a5e1a13c1bf4e23a526d631af676d95897
yoyodebian <smanzoor@umich.edu>
2025-12-01 13:17:51 -0500

    fix for diffusion

--- get_model/model/diffusion.py
+++ get_model/model/diffusion.py
@@ -100,23 +100,31 @@ class Attention(nn.Module):
 
 class Mlp(nn.Module):
     """
-    Standard MLP / FeedForward module.
+    From transformer.py
     """
-    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.):
+    def __init__(
+        self,
+        in_features,
+        hidden_features=None,
+        out_features=None,
+        act_layer=nn.GELU,
+        drop=0.0,
+    ):
         super().__init__()
         out_features = out_features or in_features
         hidden_features = hidden_features or in_features
         self.fc1 = nn.Linear(in_features, hidden_features)
         self.act = act_layer()
         self.fc2 = nn.Linear(hidden_features, out_features)
-        self.drop = nn.Dropout(drop)
+        self.drop1 = nn.Dropout(drop)
+        self.drop2 = nn.Dropout(drop)
 
     def forward(self, x):
         x = self.fc1(x)
         x = self.act(x)
-        x = self.drop(x)
+        # x = self.drop1(x)
         x = self.fc2(x)
-        x = self.drop(x)
+        x = self.drop2(x)
         return x
 
 class DiTBlock(nn.Module):
